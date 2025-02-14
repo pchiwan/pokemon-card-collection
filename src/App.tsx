47e5b8e6aef71c7ccff021cd5ad647216bdc9227
Sílvia Mur Blanch <silvia.mur-blanch@heyflow.com>
2025-02-14 21:35:20 +0100

    Initial commit: Project setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <main className="flex flex-col items-center gap-8 py-16 max-w-[1280px] mx-auto">
+      <h1 className="text-4xl font-bold">Ash's Pokémon Card Collection</h1>
+    </main>
+  );
+};
